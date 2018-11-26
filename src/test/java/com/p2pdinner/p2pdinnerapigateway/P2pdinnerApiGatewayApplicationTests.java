1e93ee2790be9f37aaf8dc295dd1e475c3fe8019
rajanikanthy <rajanikanthy@outlook.com>
2018-11-26 05:02:10 -0800

    initial commit

---
+++ src/test/java/com/p2pdinner/p2pdinnerapigateway/P2pdinnerApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.p2pdinner.p2pdinnerapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class P2pdinnerApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
