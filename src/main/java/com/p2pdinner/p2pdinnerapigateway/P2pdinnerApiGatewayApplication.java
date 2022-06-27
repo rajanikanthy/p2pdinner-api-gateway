1eac9b9893d599c8b3282a49b56ac7bee5f09cd0
rajanikanthy@outlook.com <rajanikanthy@outlook.com>
2022-06-26 21:09:52 -0700

    remove zuul and use spring cloud gateway

--- src/main/java/com/p2pdinner/p2pdinnerapigateway/P2pdinnerApiGatewayApplication.java
+++ src/main/java/com/p2pdinner/p2pdinnerapigateway/P2pdinnerApiGatewayApplication.java
@@ -4,15 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulProxy
 @EnableDiscoveryClient
 public class P2pdinnerApiGatewayApplication {
 
@@ -20,22 +14,4 @@ public class P2pdinnerApiGatewayApplication {
 		SpringApplication.run(P2pdinnerApiGatewayApplication.class, args);
 	}
 
-
-	@Bean
-	public CorsFilter corsFilter() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		final CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("HEAD");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("DELETE");
-		config.addAllowedMethod("PATCH");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
 }
